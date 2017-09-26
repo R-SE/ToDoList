5dd67c22d47145a3c4f4515f5647d0d3b167c8bc
Rose <TheBarBreaker@gmail.com>
2017-09-26 19:05:20 -0400

    v4, todoList now stores objects, not simple text items, and includes a 'completed' property; methods modified accordingly

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
+// v4: var todoList now stores objects, not simple text items; updated methods accordingly; added toggleCompleted
+
 var todoList = {
-  todos: ['task1', 'task2', 'task3', 'task4'],
+  todos: [
+      {todoText: "take out the trash", completed: false},
+      {todoText: "wash the dishes", completed: false},
+      {todoText: "make a list", completed: true}
+    ],
   displayTodos: function() {
     console.log('My Todos: ', this.todos);
   },
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  addTodo: function(todoTextValue) {
+    this.todos.push({
+      todoText: todoTextValue,
+      completed: false,
+    });
     this.displayTodos();
   },
-  changeTodo: function(index, newValue) {
-    this.todos[index-1] = newValue;
+  changeTodo: function(index, newTodoText) {
+    this.todos[index-1].todoText = newTodoText;
     this.displayTodos();
   },
   deleteTodo: function(index) {
     this.todos.splice(index-1, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(index) {
+    var todo = this.todos[index-1];
+    todo.completed = !todo.completed;
     this.displayTodos();
   }
 }
