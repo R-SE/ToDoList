b4dc061f90eea44ae583a7e59db82a279376395c
Rose <TheBarBreaker@gmail.com>
2017-09-26 17:43:56 -0400

    js code for v3, writing methods for todoList object

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//MUH CODE
+var todoList = {
+  todos: ['task1', 'task2', 'task3', 'task4'],
+  displayTodos: function() {
+    console.log('My Todos: ', this.todos);
+  },
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function(index, newValue) {
+    this.todos[index-1] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(index) {
+    this.todos.splice(index-1, 1);
+    this.displayTodos();
+  }
+}
