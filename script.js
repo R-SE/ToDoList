389f741b3f7c98cd761d276658ea986a00c4dcbd
Rose <TheBarBreaker@gmail.com>
2017-09-27 18:05:47 -0400

    v8, all methods now have buttons, but still input to the console

--- script.js
+++ script.js
@@ -55,15 +55,46 @@ var todoList = {
       }
     this.displayTodos();
   }
-    //add new functions here
 }
 
-var displayTodosButton = document.getElementById('displayTodos');
-displayTodosButton.addEventListener('click', function() {
-  todoList.displayTodos();
-});
+// uses getElementById and addEventListener calls; longer but may be more flexible in some cases
+// var displayTodosButton = document.getElementById('displayTodos');
+// displayTodosButton.addEventListener('click', function() {
+//   todoList.displayTodos();
+// });
+// var toggleAllButton = document.getElementById('toggleAll');
+// toggleAll.addEventListener('click',function() {
+//   todoList.toggleAll();
+// });
 
-var toggleAllButton = document.getElementById('toggleAll');
-toggleAll.addEventListener('click', function() {
-  todoList.toggleAll();
-});
+var handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+  },
+  addTodo: function() {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';
+  },
+  changeTodo: function() {
+    var changeTodoIndexInput = document.getElementById('changeTodoIndexInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoIndexInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoTextInput.value = '';
+    changeTodoIndexInput.value = '';
+  },
+  deleteTodo: function() {
+    var deleteTodoIndexInput = document.getElementById('deleteTodoIndexInput');
+    todoList.deleteTodo(deleteTodoIndexInput.valueAsNumber);
+    deleteTodoIndexInput.value = '';
+  },
+  toggleCompleted: function() {
+    var toggleCompletedIndexInput = document.getElementById('toggleCompletedIndexInput');
+    todoList.toggleCompleted(toggleCompletedIndexInput.valueAsNumber);
+    toggleCompletedIndexInput.value = '';
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+  }
+  //add new functions here
+}
